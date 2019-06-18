f83d783182ba3400c318eab6a8489e00503cad80
Zain Jabbar <zaijab2000@gmail.com>
2019-06-18 13:54:48 -1000

    Creating package scaffolding

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='warriorwaves',
+      version='0.1',
+      description='Python wave analysis package from Manoa',
+      url='https://github.com/Zaijab/warriorwaves',
+      author='Zain Jabbar',
+      author_email='zaijab2000@gmail.com',
+      license='MIT',
+      packages=['warriorwaves'],
+      zip_safe=False)
