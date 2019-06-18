1c441c3056a4df860f506d5a20f1c8f58ea666e1
Zain Jabbar <zaijab2000@gmail.com>
2019-06-18 13:33:01 -1000

    Moving Code from Notebook to package system

---
+++ analysistools.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+'''Class holds functions useful for waveform analysis'''
+class Analysistools:
+
+    '''Creates an array to properly baseline voltage'''
+    def baseline(self,voltagearray):
+        #TODO
+        print(1)
--- io.py
+++ io.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 import pandas as pd
 
+'''Class that handles the input of datafiles to standardize io'''
 class IO:
+    '''Constructor takes in a filename'''
     def __init__(self,filename):
-        dt = pd.read_csv(filename + '.dt',sep='\t',usecols=[1,2,3,4],skiprows=[0],names=['ch0','ch1','ch2','ch3']).values
+        self.filename = filename
+        self.dt = pd.read_csv(filename + '.dt',sep='\t',usecols=[1,2,3,4],skiprows=[0],names=['ch0','ch1','ch2','ch3']).values
+
+    '''Creates an array for all of the time series'''
+    def timeseries(self):
+        #TODO Copy function
+       print('i')
+
+    '''Creates an array from file put into constructor'''
+    def createarray(self):
         n = 0
         a = []
 
@@ -20,5 +31,5 @@ class IO:
 
                 n += 1
 
-                if (n == 8100):
+                if (n == 10):
                     return a
